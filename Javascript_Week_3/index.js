a28bd2e593c6bc356131c7b5b71e782828b5d9e6
KennethOlivas <kennetholivas1@gmail.com>
2022-02-06 11:38:12 -0600

    add week 4 katas

--- Javascript_Week_3/index.js
+++ Javascript_Week_3/index.js
@@ -77,13 +77,6 @@ const toCamelCase = (str) => {
 
 //console.log(toCamelCase("the_stealth_warrior"));
 
-
-/*
-Implement the function unique_in_order which takes as argument a sequence and returns a list of items 
-without any elements with the same value next to each other and preserving the original order of elements number or string.
-
-*/
-
 const uniqueInOrder = (iterable) => {
     let res = [];
     let arr
@@ -104,12 +97,6 @@ const uniqueInOrder = (iterable) => {
 
 //console.log(uniqueInOrder([1, 2, 2, 3, 3]));
 
-/* Given: an array containing hashes of names
-Return: a string formatted as a list of names separated by commas except for the last two names,
- which should be separated by an ampersand.
-list([ {name: 'Bart'}, {name: 'Lisa'}, {name: 'Maggie'} ])
-returns 'Bart, Lisa & Maggie'
-*/
 
 const list = (names) => {
     let res = '';
@@ -124,14 +111,7 @@ const list = (names) => {
     }
     return res;
 }
-//console.log(list([{ name: 'Bart' }, { name: 'Lisa' }, { name: 'Maggie' }]));
 
-
-/*
-
-Encrypt this! 
-
-*/
 
 const encryptThis = (text) => {
     // text = "A wise old owl lived in an oak"
@@ -152,7 +132,6 @@ const encryptThis = (text) => {
     return outputString.join(' '); //join the array into a string 
 }
 
-//console.log(encryptThis("A wise old owl lived in an oak"));
 
 
 function foldArray(array, runs) {
@@ -169,15 +148,8 @@ function foldArray(array, runs) {
 
 
 
-// add the even positions the result muptiply by 3 sum the odd positions 
-// and return last digit of the sum
-// 39847 => 7
 
 
-
-/* encode example aaabbb to 3a3b
-aabaa to 2a1b2a
-*/
 
 const encode = (str) => {
     let res = '';
@@ -194,14 +166,7 @@ const encode = (str) => {
     return res;
 }
 
-// array is an array of integers. The n1 and n2 parameters are integers defined by 
-//the relation 0 ≤= n1 <= n2 ≤ array.length.
 
-
-
-
-// return the sum of all the positive multiples of 3 or 5 or 7 stricly bellow n
-// ex: n = 11 we get 3,5,6,9,10 as mupltiples and the sum of these is 40
 
 const solution = (n) => {
     let sum = 0;
@@ -212,16 +177,6 @@ const solution = (n) => {
     }
     return sum;
 }
-
-
-///console.log(solution(15));
-
-// numeber = 39847
-// sum the digits in the even-numbered positions: 3 + 8 + 7 = 18
-// multiply the sum by 3: 18 * 3 = 54
-// sum the digits in the odd-numbered positions: 54 + (9 + 4)= 67
-// last digit of the sum: 7
-// subtract 7 from 10: 10 - 7 = 3
 
 const lastDigit = (number) => {
     let sum = 0;
@@ -243,97 +198,3 @@ const lastDigit = (number) => {
 }
 
 console.log(lastDigit(39847));
-
-
-// weak 4 
-
-const array_diff = (a, b) => {
-    return a.filter(element => !b.includes(element));
-}
-
-
-const createPhoneNumber = (numbers) => {
-    return numbers.join('').replace(/(\d{3})(\d{3})(\d{4})/, '($1) $2-$3'); 
-}
-
-console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
-
-const isPanagram = (str) => {
-    str = str.toLowerCase().split('');
-    let alphabet = 'abcdefghijklmnopqrstuvwxyz';
-    for (let i = 0; i < alphabet.length; i++) {
-        if (!str.includes(alphabet[i])) {
-            return false;
-        }
-    }
-    return true;
-}
-
-const findMissingLetter = (array) => {
-    let first = array[0].charCodeAt(0) // get the char code of the first element
-    for (let i = 1; i < array.length; i++) { // loop through the array
-      if (first + i !== array[i].charCodeAt(0)) { // if the char code of the element is not equal to the char code of the first element + the index of the element
-        return String.fromCharCode(first + i) // return the char code of the first element + the index of the element
-      }
-    }
-}
-
-
-const findUniqValue = (arr) => { 
-    return arr.filter((value) => {
-        return arr.indexOf(value) === arr.lastIndexOf(value);
-      })[0] 
-}
-
-console.log(findUniqValue([ 0, 1, 0 ]));
-
-
-
-//revrot("123456987654", 6) --> "234561876549"
-// revrot("123456987653", 6) --> "234561356789"
-// revrot("66443875", 4) --> "44668753"
-// revrot("66443875", 8) --> "64438756"
-// revrot("664438769", 8) --> "67834466"
-// revrot("123456779", 8) --> "23456771"
-// revrot("", 8) --> ""
-// revrot("123456779", 0) --> "" 
-// revrot("563000655734469485", 4) --> "0365065073456944"
-
-const revrot = (str, sz) => {
-    if (sz <= 0) {
-        return '';
-    }
-    let arr = str.split('');
-    let output = [];
-    for (let i = 0; i < arr.length; i += sz) {
-        let temp = arr.slice(i, i + sz);
-        if (temp.length === sz) {
-            let sum = 0;
-            for (let j = 0; j < temp.length; j++) {
-                sum += parseInt(temp[j]);
-            }
-            if (sum % 2 === 0) {
-                output.push(temp.reverse().join(''));
-            } else {
-                output.push(temp.slice(1).concat(temp[0]).join(''));
-            }
-        }
-    }
-    return output.join('');
-}
-
-console.log(revrot("123456987654", 6));
-
-
-
-
-
-const findThePoisoned = (rats) => {
-    // covert array to binary and the result to int
-    let binary = rats.reduce((acc, curr) => {
-        return acc + Math.pow(2, curr);
-    }, 0);
-    return binary
-}
-
-console.log(findThePoisoned([4, 2, 2, 2, 5, 1, 2, 1]));
---
+++ Javascript_Week_4/index.js
@@ -0,0 +1,82 @@
+
+// weak 4 
+
+const array_diff = (a, b) => {
+    return a.filter(element => !b.includes(element));
+}
+
+
+const createPhoneNumber = (numbers) => {
+    return numbers.join('').replace(/(\d{3})(\d{3})(\d{4})/, '($1) $2-$3'); 
+}
+
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
+
+const isPanagram = (str) => {
+    str = str.toLowerCase().split('');
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    for (let i = 0; i < alphabet.length; i++) {
+        if (!str.includes(alphabet[i])) {
+            return false;
+        }
+    }
+    return true;
+}
+
+const findMissingLetter = (array) => {
+    let first = array[0].charCodeAt(0) // get the char code of the first element
+    for (let i = 1; i < array.length; i++) { // loop through the array
+      if (first + i !== array[i].charCodeAt(0)) { // if the char code of the element is not equal to the char code of the first element + the index of the element
+        return String.fromCharCode(first + i) // return the char code of the first element + the index of the element
+      }
+    }
+}
+
+
+const findUniqValue = (arr) => { 
+    return arr.filter((value) => {
+        return arr.indexOf(value) === arr.lastIndexOf(value);
+      })[0] 
+}
+
+console.log(findUniqValue([ 0, 1, 0 ]));
+
+
+const revrot = (str, sz) => {
+    if (sz <= 0) {
+        return '';
+    }
+    let arr = str.split('');
+    let output = [];
+    for (let i = 0; i < arr.length; i += sz) {
+        let temp = arr.slice(i, i + sz);
+        if (temp.length === sz) {
+            let sum = 0;
+            for (let j = 0; j < temp.length; j++) {
+                sum += parseInt(temp[j]);
+            }
+            if (sum % 2 === 0) {
+                output.push(temp.reverse().join(''));
+            } else {
+                output.push(temp.slice(1).concat(temp[0]).join(''));
+            }
+        }
+    }
+    return output.join('');
+}
+
+console.log(revrot("123456987654", 6));
+
+
+
+
+
+const findThePoisoned = (rats) => {
+    // covert array to binary and the result to int
+    let binary = rats.reduce((acc, curr) => {
+        return acc + Math.pow(2, curr);
+    }, 0);
+    return binary
+}
+
+console.log(findThePoisoned([4, 2, 2, 2, 5, 1, 2, 1]));
